bf56210827050f7478583ba460abe1357bd8c8a4
Michael Martinelli <turtelboy123@gmail.com>
2022-01-12 20:16:06 -0700

    Inital commit

---
+++ challenge.js
@@ -0,0 +1,9 @@
+var variableOne = ("Variable");
+var variableTwo = 2;
+var variableThree = true;
+var variableFour = ('Another Variable');
+var variableFive = 5;
+var variableSix = false;
+var arrayVariable = [];
+//I chose this name so it can specify the challenge fodler.
+//
---
+++ challenge/index.md
@@ -0,0 +1,5 @@
+<h1> Header </h2>
+-To Kill a Mockingbird
+-The Picture of Dorian Gray
+-Lord of the Rings
+-Winds of Winter 
